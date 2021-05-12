40213f1ebae589fb9f70e37a488d94828fb8b39c
Jayshawn <Jayshawngrant1@gmail.com>
2021-05-11 20:46:37 -0400

    Initial Commit

---
+++ SimpleList/SimpleListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleListApp.swift
+//  SimpleList
+//
+//  Created by Jayshawn Grant on 5/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SimpleListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
